d48c0d392847999213289935e05b3784663d185c
Ben Smyth <118232312+bsmyth310@users.noreply.github.com>
2023-04-29 05:54:57 -0400

    Add files via upload
    Fixed Utils location

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import Header from './components/Header/Header.js';
-import Footer from './components/Footer/index.js';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Footer />
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
     </div>
   );
 }
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,9 +1,5 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.cs";
-
-function Wrapper(props) {
-  return <div className="wrapper">{props.children}</div>
-}
+import 'bootstrap/dist/css/bootstrap.min.cs';
 
 function Portfolio (){
 
@@ -12,12 +8,6 @@ function Portfolio (){
             <div className="project">
                 <h2 className="top-title">Bootcamp Portfolio</h2>
             </div>
-
-            <Wrapper id="card-data">
-                {portfolio.map((project) => (
-                <ProjectCards key={project.id} image={project.image} name={project.name} github={project.github} deploy={project.deploy} topics={project.topics}/>
-                ))}
-            </Wrapper>
         </section>
     )
 }
