e85d459dd8166f81efaacf752110f350892a6138
Ben Smyth <118232312+bsmyth310@users.noreply.github.com>
2023-05-10 13:32:52 +0000

    Pending changes exported from your codespace

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import React from "react";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import "./App.css";
-import "bootstrap/dist/css/bootstrap.css";
+import { Routes, Route } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Layout from './components/Layout'
+import Portfolio from './components/Portfolio'
+import Dashboard from './components/Dashboard'
+import './App.scss';
+
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Footer />
-    </div>
+   <>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Route>
+    </Routes>
+   </>
   );
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom'; // 'react-dom/client' if 'react-dom' doesn't work
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+    <BrowserRouter>
+      <App />
+      </BrowserRouter>
+      ,
+  </React.StrictMode>,
+  document.getElementById('root')
+)
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+//root.render(
+  //<React.StrictMode>
+    //<App />
+  //</React.StrictMode>
+//);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
